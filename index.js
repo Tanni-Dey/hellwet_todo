b8bbe67580835d0103b87bc25f1ee07df44d2aa6
Tanni-Dey <tonnidey277615@gmail.com>
2023-04-04 14:05:16 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1tyqf.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    //all colletction
+    const todoCollection = client.db("hellwettask").collection("todos");
+
+    app.get("/todos", async (req, res) => {
+      const query = {};
+      const cursor = todoCollection.find(query);
+      const allTodo = await cursor.toArray(cursor);
+      res.send(allTodo);
+    });
+  } finally {
+  }
+}
+
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hellwet Task");
+});
+
+app.listen(port, () => {
+  console.log("Hellwet Task Connected", port);
+});
